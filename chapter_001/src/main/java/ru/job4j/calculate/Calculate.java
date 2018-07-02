bd1ed401bced86cb5ed4bc681fc3d8f5972e4006
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2018-07-02 07:29:52 +0300

    Решение задачи 1.3 JavaDoc Code Convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Класс Calculate для решения задачи из час 001 урок 1.
+ @author agoryunov
+ @since 01.07.2018
+*/
+
 public class Calculate {
+	/**
+	*Main
+	@param args - args
+	*/	
 	public static void main(String[] args){
 			System.out.println("Hello world");
 	}
