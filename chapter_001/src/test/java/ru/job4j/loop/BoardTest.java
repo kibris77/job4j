819c4ca86447b42b0fbe6e34c5fd51dc263fb6b8
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2018-07-06 08:23:05 +0300

    Решение задачи 5.3 построить шахматную доску

---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import ru.job4j.loop.Board;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author Gorunov Alexandr (mailto:gorunov.sasha85@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BoardTest {
+    @Test
+    public void when3x3() {
+        Board board = new Board();
+        String rsl = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(
+                String.format("X X%s X %sX X%s", ln, ln, ln)
+                )
+        );
+    }
+
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        //напишите здесь тест, проверяющий формирование доски 5 на 4.
+        Board board = new Board();
+        String rsl = board.paint(5, 4);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(
+                String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)
+                )
+        );
+
+    }
+
+}
