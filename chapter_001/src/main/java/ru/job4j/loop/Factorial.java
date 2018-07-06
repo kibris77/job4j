f8c8c76daf2336d4073e6b818b190825bdafb0dc
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2018-07-06 07:41:06 +0300

    Решение задачи 5.2. Создать программу вычисляющую факториал

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * @author Gorunov Alexandr(mailto:gorunov.sasha85@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Factorial {
+    /**
+     * Метод вычисляет факториал числа
+     * @param n - число
+     * @return Факторил
+     */
+    public int calc(int n) {
+        int fact = 1;
+
+        if (n == 0) {
+            return fact;
+        }
+
+        for (int i = 1; i < n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
