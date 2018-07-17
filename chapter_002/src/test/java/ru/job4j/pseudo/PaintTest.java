12c1ef5dc0227b3df9b65803845e0b5c4e729366
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2018-07-17 08:06:01 +0300

    Рефакторинг кода теста для шаблона проектирования - стратегия.

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,17 +1,33 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
 public class PaintTest {
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.outStream));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outStream));
         new Paint().draw(new Triangle());
         assertThat(new String(outStream.toByteArray()), is(new StringBuilder()
                 .append("  *  \n")
@@ -21,9 +37,6 @@ public class PaintTest {
 
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outStream));
         new Paint().draw(new Square());
         assertThat(new String(outStream.toByteArray()), is(new StringBuilder()
                 .append("*****\n")
