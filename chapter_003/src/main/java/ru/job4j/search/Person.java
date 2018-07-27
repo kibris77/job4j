b1a12042c32387f7894601ac4216744f93fe6c1e
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2018-07-27 06:53:07 +0300

    Задача - Телефонный справочник на базе ArrayList.

---
+++ chapter_003/src/main/java/ru/job4j/search/Person.java
@@ -0,0 +1,35 @@
+package ru.job4j.search;
+
+/**
+ * Класс описывает данные человека содержащиеся в телефлнном справочнике.
+ */
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
+
